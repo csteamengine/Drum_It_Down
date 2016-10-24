5e183e75dabbc568bfbf80fd29e1f003e477c335
Gregory Steenhagen <gregory@Gregorys-iMac.local>
2016-10-23 21:46:00 -0500

    Cleaned up some code. File uploads work, and you can click to specify which spotify song corresponds to the file you uploaded. Doesnt process yet though.

--- includes/js/shared.js
+++ includes/js/shared.js
@@ -1,11 +1,7 @@
 /**
  * Created by adm_gcs on 10/19/2016.
  */
-var audioObject;
 
-/**
- * Created by adm_gcs on 6/9/2016.
- */
 
 var drum_numbers = {
     35: "Bass Drum 2",
@@ -61,56 +57,140 @@ var drum_numbers = {
 function parse_midi(file_name, original_file_name){
     file_name = "midi_files/"+file_name;
     loadRemote(file_name, function(data) {
-        midiFile = MidiFile(data);
+        var midiFile = MidiFile(data);
         midiFile.header.original_file_name = original_file_name;
-        synth = Synth(44100);
-        replayer = Replayer(midiFile, synth);
-        var found = false;
-        var deltaTime = 0;
-        var curr_delta = 0;
+        guess_track_info(midiFile);
+        return midiFile;
+    });
+}
 
-        if(midiFile.header.drum_track != null){
-            var instruments = new Array();
-            for(i =0;i< midiFile.tracks[midiFile.header.drum_track].length;i++){
-                if(midiFile.tracks[midiFile.header.drum_track][i].noteNumber != null && instruments.indexOf(midiFile.tracks[midiFile.header.drum_track][i].noteNumber) == -1){
-                    instruments.push(midiFile.tracks[midiFile.header.drum_track][i].noteNumber);
+//    This goes through all of the events with subtype trackName and queries spotify to see if any of those trackNames are a relevant title
+//    Eventually, this information will be put in the dom, asking them if the midi file is any of the following tracks.
+//    If not, they can search Spotify by searching the song name.
+function guess_track_info(midiFile) {
+    var songGuesses = $('#song_guesses');
+    songGuesses.html('');
+    if (midiFile != null) {
+        console.log(midiFile);
+        //An array that I'm storing all the spotify result in. Not going to work for the final product, due to cl
+        var top_hits = [];
+        var search_term = "";
+        var limit = 1;
+        for (var i = 0; i < midiFile.tracks.length; i++) {
+            for (var j = 0; j < midiFile.tracks[i].length; j++) {
+                if (midiFile.tracks[i][j].subtype == "trackName" && midiFile.tracks[i][j].text != "") {
+                    search_term = encodeURI(midiFile.tracks[i][j].text.trim());
+                    var url = "https://api.spotify.com/v1/search?q=" + search_term + "&type=track&limit=" + limit;
+                    $.getJSON(url, function (result) {
+
+                        if (result.tracks.items.length > 0) {
+//                                top_hits.push(result.tracks.items[0].name + " -- " + result.tracks.items[0].artists[0].name);
+                            songGuesses.append(
+                                '<div class="guess" >' + result.tracks.items[0].name + " -- " + result.tracks.items[0].artists[0].name + '<button onclick="specify_song(\'' + result.tracks.items[0].id + '\')">Select</button></div>'
+                            );
+                        }
+                    });
                 }
             }
         }
+        //TODO eventually pass in more info.
+        if (midiFile.header.original_file_name != null) {
+            limit = 5;
+            search_term = encodeURI(midiFile.header.original_file_name.trim().replace('.mid', ''));
+            search_term = search_term.replace(' ', '%20');
+            url = "https://api.spotify.com/v1/search?q=" + search_term + "&type=track&limit=" + limit;
+            $.getJSON(url, function (result) {
+                if (result.tracks.items.length > 0) {
+                    for (m = 0; m < result.tracks.items.length; m++) {
+//                            top_hits.push(result.tracks.items[m].name + " -- " + result.tracks.items[m].artists[0].name );
+                        $('#song_guesses').append(
+                            '<div class="guess" >' + result.tracks.items[m].name.trim() + " -- " + result.tracks.items[m].artists[0].name.trim() + '<button onclick="specify_song(\'' + result.tracks.items[m].id + '\')">Select</button> </div>'
+                        );
+                    }
+                }
 
-        // console.log(midiFile);
-        // console.log(instruments);
-        // for(i=0;i<instruments.length;i++){
-        //     console.log(drum_numbers[instruments[i]]);
-        // }
-        // synth = Synth(44100);
-        // replayer = Replayer(midiFile, synth);
-        // audio = AudioPlayer(replayer);
-        // audio.play();
-        // // play_drums(midiFile,milliseconds);
-        // var drums = midiFile.header['drum_track'];
-        //
-        // if(found){
-        //     var counter = 0;
-        //     var timer = setInterval(function(){
-        //         counter++;
-        //         if(counter == 100){
-        //             clearInterval(timer);
-        //         }
-        //     },milliseconds);
-        // }else{
-        //     console.log("Could not find BPM");
-        // }
+            });
+        }
+    }
+}
 
-        return midiFile;
+function upload_file() {
+
+    var file_data = $('#midi_file').prop('files')[0];
+    var form_data = new FormData();
+    form_data.append('midi_file', file_data);
+
+
+    $.ajax({
+        url: '/includes/php/upload.php?action=upload', // point to server-side PHP script
+
+        dataType: 'text',  // what to expect back from the PHP script, if anything
+        cache: false,
+        contentType: false,
+        processData: false,
+        data: form_data,
+        type: 'post',
+        success: function (php_script_response) {
+            if (php_script_response != "ERROR UPLOADING FILE") {
+                midiFile = parse_midi(php_script_response, $('#midi_file').prop('files')[0].name);
+                if (midiFile == "Error: Could not find a drum track.") {
+                    console.log("Error");
+                } else {
+                    //parsing was successful
+                    guess_track_info(midiFile);
+                }
+            }
+        }
     });
-
 }
 
 
-function play_note(midiNote){
-    console.log(midiNote.subtype + " - " + midiNote.noteNumber + " - " + midiNote.deltaTime);
+
+function search_song() {
+    var songGuesses = $('#song_guesses');
+    var songTitle = $('#song_title');
+    songGuesses.html('');
+
+    if (songTitle.val() != "") {
+        var limit = 10;
+        var search_term = encodeURI(songTitle.val());
+        var url = "https://api.spotify.com/v1/search?q=" + search_term + "&type=track&limit=" + limit;
+        $.getJSON(url, function (result) {
+            if (result.tracks.items.length > 0) {
+                for (var m = 0; m < result.tracks.items.length; m++) {
+//                        console.log(result.tracks.items[m].name + " -- " + result.tracks.items[m].artists[0].name );
+                    songGuesses.append(
+                        '<div class="guess" >' + result.tracks.items[m].name.trim() + " -- " + result.tracks.items[m].artists[0].name.trim() + '<button onclick="specify_song(\'' + result.tracks.items[m].id + '\')">Select</button></div>'
+                    );
+                }
+            }
+
+        });
+    }
 }
+
+function specify_song(id) {
+    $('#song_guesses').html('');
+
+    var url = "https://api.spotify.com/v1/tracks/" + id;
+    $.getJSON(url, function (result) {
+
+        var new_url = "includes/php/upload.php?action=rename&title=" + encodeURI(result.name) + "&artist=" + encodeURI(result.artists[0].name) + "&album=" +
+            encodeURI(result.album.name) + "&duration=" + encodeURI(result.duration_ms) + "&spotify_id=" + encodeURI(result.id) + "&spotify_url=" + encodeURI(result.href) + "&spotify_popularity=" +
+            encodeURI(result.popularity) + "&preview_url=" + encodeURI(result.preview_url) + "&spotify_uri=" + encodeURI(result.uri) + "&image_url=" + encodeURI(result.album.images[0].url);
+
+        $.ajax({
+            url: new_url,
+            dataType: 'text',  // what to expect back from the PHP script, if anything
+            cache: false,
+            success: function (php_script_response) {
+                console.log(php_script_response);
+            }
+        });
+
+    });
+}
+
 function loadRemote(path, callback) {
     var fetch = new XMLHttpRequest();
     fetch.open('GET', path);
@@ -127,6 +207,6 @@ function loadRemote(path, callback) {
             }
             callback(ff.join(""));
         }
-    }
+    };
     fetch.send();
 }
--- index.php
+++ index.php
@@ -16,8 +16,6 @@ include "includes/php/general.php";
 
 
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -34,8 +32,8 @@ include "includes/php/general.php";
     <script src='includes/jasmid/stream.js'></script>
     <script type="application/javascript" src="includes/jasmid/midifile.js"></script>
     <script src="includes/jasmid/replayer.js"></script>
-    <script src="includes/jasmid/synth.js"></script>
-    <script src="includes/jasmid/audio.js"></script>
+<!--    <script src="includes/jasmid/synth.js"></script>-->
+<!--    <script src="includes/jasmid/audio.js"></script>-->
     <script src="/includes/js/jquery-2.2.4.min.js" type="application/javascript"></script>
 
 </head>
@@ -82,137 +80,7 @@ include "includes/php/footer.php";
             event.preventDefault();
             search_song();
         });
-        function upload_file() {
 
-            var file_data = $('#midi_file').prop('files')[0];
-            var form_data = new FormData();
-            form_data.append('midi_file', file_data);
-
-
-            $.ajax({
-                url: '/includes/php/upload.php?action=upload', // point to server-side PHP script
-
-                dataType: 'text',  // what to expect back from the PHP script, if anything
-                cache: false,
-                contentType: false,
-                processData: false,
-                data: form_data,
-                type: 'post',
-                success: function (php_script_response) {
-                    if (php_script_response != "ERROR UPLOADING FILE") {
-                        parse_midi(php_script_response, $('#midi_file').prop('files')[0].name);
-                        if (midiFile == "Error: Could not find a drum track.") {
-                            console.log("Error");
-                        } else {
-                            //parsing was successful
-
-                        }
-                    }
-                }
-            });
-
-            setTimeout(function () {
-                guess_track_info();
-            }, 1000);
-
-        }
-
-        //This goes through all of the events with subtype trackName and queries spotify to see if any of those trackNames are a relevant title
-        //    Eventually, this information will be put in the dom, asking them if the midi file is any of the following tracks.
-        //    If not, they can search Spotify by searching the song name.
-        function guess_track_info() {
-            var songGuesses = $('#song_guesses');
-            songGuesses.html('');
-            if (midiFile != null) {
-                console.log(midiFile);
-                //An array that I'm storing all the spotify result in. Not going to work for the final product, due to cl
-                var top_hits = [];
-                var search_term = "";
-                var limit = 1;
-                for (var i = 0; i < midiFile.tracks.length; i++) {
-                    for (var j = 0; j < midiFile.tracks[i].length; j++) {
-                        if (midiFile.tracks[i][j].subtype == "trackName" && midiFile.tracks[i][j].text != "") {
-                            search_term = encodeURI(midiFile.tracks[i][j].text.trim());
-                            var url = "https://api.spotify.com/v1/search?q=" + search_term + "&type=track&limit=" + limit;
-                            $.getJSON(url, function (result) {
-
-                                if (result.tracks.items.length > 0) {
-//                                top_hits.push(result.tracks.items[0].name + " -- " + result.tracks.items[0].artists[0].name);
-                                    songGuesses.append(
-                                        '<div class="guess" >' + result.tracks.items[0].name + " -- " + result.tracks.items[0].artists[0].name + '<button onclick="specify_song(\'' + result.tracks.items[0].id + '\')">Select</button></div>'
-                                    );
-                                }
-                            });
-                        }
-                    }
-                }
-                //TODO eventually pass in more info.
-                if (midiFile.header.original_file_name != null) {
-                    limit = 5;
-                    search_term = encodeURI(midiFile.header.original_file_name.trim().replace('.mid', ''));
-                    search_term = search_term.replace(' ', '%20');
-                    url = "https://api.spotify.com/v1/search?q=" + search_term + "&type=track&limit=" + limit;
-                    $.getJSON(url, function (result) {
-                        if (result.tracks.items.length > 0) {
-                            for (m = 0; m < result.tracks.items.length; m++) {
-//                            top_hits.push(result.tracks.items[m].name + " -- " + result.tracks.items[m].artists[0].name );
-                                $('#song_guesses').append(
-                                    '<div class="guess" >' + result.tracks.items[m].name.trim() + " -- " + result.tracks.items[m].artists[0].name.trim() + '<button onclick="specify_song(\'' + result.tracks.items[m].id + '\')">Select</button> </div>'
-                                );
-                            }
-                        }
-
-                    });
-                }
-
-
-            }
-        }
-
-        function search_song() {
-            var songGuesses = $('#song_guesses');
-            var songTitle = $('#song_title');
-            songGuesses.html('');
-
-            if (songTitle.val() != "") {
-                var limit = 10;
-                var search_term = encodeURI(songTitle.val());
-                var url = "https://api.spotify.com/v1/search?q=" + search_term + "&type=track&limit=" + limit;
-                $.getJSON(url, function (result) {
-                    if (result.tracks.items.length > 0) {
-                        for (var m = 0; m < result.tracks.items.length; m++) {
-//                        console.log(result.tracks.items[m].name + " -- " + result.tracks.items[m].artists[0].name );
-                            songGuesses.append(
-                                '<div class="guess" >' + result.tracks.items[m].name.trim() + " -- " + result.tracks.items[m].artists[0].name.trim() + '<button onclick="specify_song(\'' + result.tracks.items[m].id + '\')">Select</button></div>'
-                            );
-                        }
-                    }
-
-                });
-            }
-        }
-
-        function specify_song(id) {
-            $('#song_guesses').html('');
-
-            var url = "https://api.spotify.com/v1/tracks/" + id;
-            $.getJSON(url, function (result) {
-
-                var new_url = "includes/php/upload.php?action=rename&title=" + encodeURI(result.name) + "&artist=" + encodeURI(result.artists[0].name) + "&album=" +
-                    encodeURI(result.album.name) + "&duration=" + encodeURI(result.duration_ms) + "&spotify_id=" + encodeURI(result.id) + "&spotify_url=" + encodeURI(result.href) + "&spotify_popularity=" +
-                    encodeURI(result.popularity) + "&preview_url=" + encodeURI(result.preview_url) + "&spotify_uri=" + encodeURI(result.uri) + "&image_url=" + encodeURI(result.album.images[0].url);
-
-                $.ajax({
-                    url: new_url,
-                    dataType: 'text',  // what to expect back from the PHP script, if anything
-                    cache: false,
-                    success: function (php_script_response) {
-                        console.log(php_script_response);
-                    }
-                });
-
-            });
-        }
     });
 </script>
 </html>
